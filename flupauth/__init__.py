cf090559170929fbff391e525d48577fbc6524ee
Allan Saddi <allan@saddi.com>
2018-05-03 00:07:18 -0700

    Wrap top-level imports in case dependencies haven't been installed.

--- flupauth/__init__.py
+++ flupauth/__init__.py
@@ -1,4 +1,13 @@
 from _authinfo import *
-from _oidc import *
-from _steam import *
+
+try:
+    from _oidc import *
+except ImportError:
+    pass
+
+try:
+    from _steam import *
+except ImportError:
+    pass
+
 from _cas import *
